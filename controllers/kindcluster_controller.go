fce406949957af61d9348e61e8bfc9c5245ea0b5
Manuel Bovo <manuel.bovo@gmail.com>
2022-10-26 19:54:06 +0200

    feat(reconcile): (WIP) (#7)
    * feat(reconcile): WIP preparing fields and main logic
    * feat(reconcile): WIP empy reconciliation logic
    * feat(reconcile): adding watcher for Cluster resourcers referencing KindClusters
    * feat(reconcile): adhere to Api version labels
    * WIP: goreleaser
    * Adding clusterctl templates and goreleaser
    * WIP: fix typo and re-generate assets
    * WIP: adhere to provider contract
    * WIP: fix release
    * WIP: make tests only on PR

--- controllers/kindcluster_controller.go
+++ controllers/kindcluster_controller.go
@@ -18,24 +18,47 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/go-logr/logr"
+	"github.com/mbovo/cluster-api-provider-kind/api/v1alpha1"
+	infrastructurev1alpha1 "github.com/mbovo/cluster-api-provider-kind/api/v1alpha1"
+	"github.com/pkg/errors"
+
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/annotations"
+	"sigs.k8s.io/cluster-api/util/patch"
+	"sigs.k8s.io/cluster-api/util/predicates"
+
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
-	infrastructurev1alpha1 "github.com/mbovo/cluster-api-provider-kind/api/v1alpha1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
 // KindClusterReconciler reconciles a KindCluster object
 type KindClusterReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme   *runtime.Scheme
+	Recorder record.EventRecorder
 }
 
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=kindclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=kindclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=kindclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -46,17 +69,134 @@ type KindClusterReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
-func (r *KindClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+func (r *KindClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (_ ctrl.Result, err error) {
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	kindCluster := &infrastructurev1alpha1.KindCluster{}
+	if err := r.Client.Get(ctx, req.NamespacedName, kindCluster); err != nil {
+		logger.Info(fmt.Sprintf("Failed to get KindClusteter resource '%s/%s'.", req.NamespacedName.Namespace, req.NamespacedName.Name))
+		return reconcile.Result{}, client.IgnoreNotFound(err)
+	}
 
-	return ctrl.Result{}, nil
+	// Fetch the Cluster:
+	cluster, err := util.GetOwnerCluster(ctx, r.Client, kindCluster.ObjectMeta)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	if cluster == nil {
+		logger.Info("Cluster Controller has not yet set OwnerRef")
+		return reconcile.Result{Requeue: true, RequeueAfter: 5 * time.Second}, nil
+	}
+
+	if annotations.IsPaused(cluster, kindCluster) {
+		r.Recorder.Eventf(kindCluster, corev1.EventTypeNormal, "ClusterPaused", "Cluster is paused")
+		logger.Info("KindCluster or linked Cluster is marked as paused, will not reconcile")
+		return reconcile.Result{}, nil
+	}
+
+	// Handle deleted clusters
+	if !kindCluster.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(ctx, kindCluster)
+	}
+
+	// Handle non-deleted clusters
+	return r.reconcileNormal(ctx, kindCluster)
+}
+
+func (r *KindClusterReconciler) reconcileNormal(ctx context.Context, kindCluster *v1alpha1.KindCluster) (reconcile.Result, error) {
+	logger := log.FromContext(ctx)
+	_, err := patch.NewHelper(kindCluster, r.Client)
+	if err != nil {
+		log.Log.Error(err, "cannot create patch helper")
+		return reconcile.Result{}, err
+	}
+	logger.Info("Reconciling KindCluster")
+
+	return reconcile.Result{}, nil
+}
+
+func (r *KindClusterReconciler) reconcileDelete(ctx context.Context, kindCluster *v1alpha1.KindCluster) (reconcile.Result, error) {
+	logger := log.FromContext(ctx)
+	_, err := patch.NewHelper(kindCluster, r.Client)
+	if err != nil {
+		log.Log.Error(err, "cannot create patch helper")
+		return reconcile.Result{}, err
+	}
+	logger.Info("Reconciling deleted KindCluster")
+
+	return reconcile.Result{}, nil
+}
+
+// Filter out change of status subresource
+func filterStatusChanges() predicate.Predicate {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			// Ignore custom resource updates when metadata.Generation does not change
+			// i.e. ignore change of evrything except "spec" field (https://github.com/kubernetes/kubernetes/issues/67428#issuecomment-456125985)
+			return e.ObjectOld.GetGeneration() != e.ObjectNew.GetGeneration()
+		},
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
-func (r *KindClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
+func (r *KindClusterReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
+	log := ctrl.LoggerFrom(ctx)
+
+	controller, err := ctrl.NewControllerManagedBy(mgr).
 		For(&infrastructurev1alpha1.KindCluster{}).
-		Complete(r)
+		WithEventFilter(predicates.ResourceNotPaused(log)).
+		WithEventFilter(predicates.ResourceIsNotExternallyManaged(log)).
+		WithEventFilter(filterStatusChanges()).
+		Build(r)
+
+	if err != nil {
+		return errors.Wrap(err, "failed setting up with a controller manager")
+	}
+
+	// Watch for changes to Cluster resources to enque referenced KindCluster resources for reconciliation.
+	return controller.Watch(
+		&source.Kind{Type: &clusterv1.Cluster{}},
+		handler.EnqueueRequestsFromMapFunc(r.mapClusterToKindCluster(ctx, log)),
+		predicates.ClusterUnpaused(log))
+}
+
+// Map a KindCluster to the Cluster that is referencing it.
+func (r *KindClusterReconciler) mapClusterToKindCluster(ctx context.Context, log logr.Logger) handler.MapFunc {
+	return func(o client.Object) []ctrl.Request {
+
+		c, ok := o.(*clusterv1.Cluster)
+		if !ok {
+			log.Error(errors.Errorf("expected a Cluster but got a %T", o), "failed to get KindCluster for Cluster")
+			return nil
+		}
+		log = log.WithValues("func", "mapClusterToKindCluster", "cluster", c.Name, "namespace", c.Namespace)
+
+		if !c.ObjectMeta.DeletionTimestamp.IsZero() {
+			log.V(3).Info("Ignoring deleting Cluster")
+			return nil
+		}
+
+		if c.Spec.InfrastructureRef.GroupVersionKind().Kind != "KindCluster" {
+			log.V(3).Info("Cluster Has an InfrastructureRef of a wrong type, skipping resource")
+			return nil
+		}
+
+		kindCluster := &infrastructurev1alpha1.KindCluster{}
+		namspacedName := types.NamespacedName{Namespace: c.Spec.InfrastructureRef.Namespace, Name: c.Spec.InfrastructureRef.Name}
+
+		if err := r.Client.Get(ctx, namspacedName, kindCluster); err != nil {
+			log.V(3).Info("InfrastructureRef field not set yet, skipping resource")
+			return nil
+		}
+
+		if annotations.IsExternallyManaged(kindCluster) {
+			log.V(3).Info("Ignoring externally managed KindCluster")
+			return nil
+		}
+
+		log.V(3).Info("Enqueueing request for KindCluster", c.Spec.InfrastructureRef.Name)
+		return []ctrl.Request{
+			{NamespacedName: client.ObjectKey{Namespace: c.Spec.InfrastructureRef.Namespace, Name: c.Spec.InfrastructureRef.Name}},
+		}
+	}
 }
--- main.go
+++ main.go
@@ -89,10 +89,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Early initialization of a context with signal handlers
+	ctx := ctrl.SetupSignalHandler()
+
 	if err = (&controllers.KindClusterReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
+	}).SetupWithManager(ctx, mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "KindCluster")
 		os.Exit(1)
 	}
@@ -108,7 +111,7 @@ func main() {
 	}
 
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
